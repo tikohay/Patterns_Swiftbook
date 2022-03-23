7f4aa7311639a0903bd4585a4706838c742b67e3
Tigran Karakhanyan <lev.98@bk.ru>
2022-03-23 16:32:24 +0400

    Proxy

---
+++ Patterns_Swiftbook/Patterns/Proxy.swift
@@ -0,0 +1,73 @@
+//
+//  Proxy.swift
+//  Patterns_Swiftbook
+//
+//  Created by Karakhanyan Tigran on 23.03.2022.
+//
+
+import Foundation
+
+//protocol ServerProtocol {
+//    func grandAccess(user: User)
+//    func denyAccess(user: User)
+//}
+//
+//class ServerSide: ServerProtocol {
+//    func grandAccess(user: User) {
+//        print("access granted to user with id = \(user.id)")
+//    }
+//
+//    func denyAccess(user: User) {
+//        print("access denied to user with id = \(user.id)")
+//    }
+//}
+//
+//class ServerProxy: ServerProtocol {
+//    lazy private var server: ServerSide = ServerSide()
+//
+//    func grandAccess(user: User) {
+//        server.grandAccess(user: user)
+//    }
+//
+//    func denyAccess(user: User) {
+//        server.denyAccess(user: user)
+//    }
+//}
+//
+//class User {
+//    let id = "123"
+//}
+
+protocol ServerProtocol {
+    func grantAccess(user: User)
+}
+
+class ServerSide: ServerProtocol {
+    func grantAccess(user: User) {
+        print("access granted to user with id \(user.name)")
+    }
+}
+
+class ServerProxy: ServerProtocol {
+    private var server: ServerSide!
+    
+    func grantAccess(user: User) {
+        guard server != nil else {
+            print("access can't be granted")
+            return
+        }
+        server.grantAccess(user: user)
+    }
+    
+    func authenticate(user: User) {
+        guard user.password == "123" else { return }
+        print("user authenticated")
+        server = ServerSide()
+    }
+}
+
+class User {
+    let name = "Petr"
+    let password = "123"
+}
+
